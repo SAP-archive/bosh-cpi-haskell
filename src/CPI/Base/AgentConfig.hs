7a4ced95ed4d24c647ece8dca205946df7a67191
Jan von Loewenstein <jan.von.loewenstein@sap.com>
2017-02-07 10:19:10 +0100

    Add 'AgentSettings' module
    - (de-)serialize agent settings
    - add persistent disk

---
+++ src/CPI/Base/AgentConfig.hs
@@ -0,0 +1,141 @@
+{-# LANGUAGE DeriveAnyClass  #-}
+{-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE RankNTypes      #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module CPI.Base.AgentConfig(
+    AgentSettings(..)
+  , AgentId(..)
+  , agentId
+  , blobstore
+  , disks
+  , env
+  , networks
+  , ntp
+  , mbus
+  , vm
+  , Blobstore(..)
+  , Disks(..)
+  , system
+  , ephemeral
+  , persistent
+  , Network(..)
+  , Vm(..)
+  , name
+  , trustedCerts
+  , parseSettings
+  , addPersistentDisk
+) where
+
+import           CPI.Base
+
+import           Control.Exception.Safe
+import           Control.Lens
+import           Control.Lens.TH
+import           Data.Aeson
+import           Data.Aeson.TH
+import           Data.Aeson.Types
+import           Data.ByteString        (ByteString)
+import           Data.ByteString.Lazy   (fromStrict)
+import           Data.HashMap.Strict    (HashMap)
+import           Data.Text              (Text)
+import           GHC.Generics
+
+parseSettings :: (MonadThrow m) => ByteString -> m AgentSettings
+parseSettings raw =
+  either
+    (\msg -> throw $ CloudError $ "Could not parse agent settings " ++ msg)
+    return
+    (eitherDecode' $ fromStrict raw)
+
+data AgentSettings = AgentSettings {
+    _agentId      :: AgentId
+  , _blobstore    :: Blobstore
+  , _disks        :: Maybe Disks
+  , _env          :: HashMap Text Text
+  , _networks     :: HashMap Text Network
+  , _ntp          :: [Text]
+  , _mbus         :: Text
+  , _vm           :: Vm
+  , _trustedCerts :: Maybe Text
+} deriving (Eq, Show)
+
+newtype AgentId = AgentId Text
+    deriving (Eq, Show)
+newtype Blobstore = Blobstore (HashMap Text Value)
+    deriving (Eq, Show)
+data Disks = Disks {
+    _system     :: Text
+  , _ephemeral  :: Maybe Text
+  , _persistent :: HashMap Text Text
+} deriving (Eq, Show)
+newtype Network = Network (HashMap Text Value)
+    deriving (Eq, Show)
+data Vm = Vm {
+  _name :: Text
+} deriving (Eq, Show)
+
+makeLenses ''AgentSettings
+makeLenses ''AgentId
+makeLenses ''Blobstore
+makeLenses ''Disks
+makeLenses ''Network
+makeLenses ''Vm
+
+$(deriveJSON defaultOptions{fieldLabelModifier = drop 1} ''AgentSettings)
+$(deriveJSON defaultOptions{fieldLabelModifier = drop 1} ''AgentId)
+$(deriveJSON defaultOptions{fieldLabelModifier = drop 1} ''Blobstore)
+$(deriveJSON defaultOptions{fieldLabelModifier = drop 1} ''Disks)
+$(deriveJSON defaultOptions{fieldLabelModifier = drop 1} ''Network)
+$(deriveJSON defaultOptions{fieldLabelModifier = drop 1} ''Vm)
+
+addPersistentDisk :: AgentSettings -> Text -> Text -> AgentSettings
+addPersistentDisk settings diskId path = settings & disks._Just.persistent.at diskId.non "" .~ path
+  -- Example settings.json
+  -- {
+  --   "agent_id": "agent-xxxxxx",
+  --   "blobstore": {
+  --     "provider": "local",
+  --     "options": {
+  --       "endpoint": "http://xx.xx.xx.xx:25250",
+  --       "password": "password",
+  --       "blobstore_path": "/var/vcap/micro_bosh/data/cache",
+  --       "user": "agent"
+  --     }
+  --   },
+  --   "disks": {
+  --     "system": "/dev/xvda",
+  --     "ephemeral": "/dev/sdb",
+  --     "persistent": {}
+  --   },
+  --   "env": {},
+  --   "networks": {
+  --     "default": {
+  --       "type": "manual",
+  --       "ip": "10.234.228.158",
+  --       "netmask": "255.255.255.192",
+  --       "cloud_properties": {"name": "3112 - preprod - back"},
+  --       "dns": [
+  --         "10.234.50.180",
+  --         "10.234.71.124"
+  --       ],
+  --       "gateway": "10.234.228.129",
+  --       "mac": null
+  --     }
+  --   },
+  --   "ntp": [],
+  --   "mbus": "nats://nats:nats-password@yy.yy.yyy:4222",
+  --   "vm": {"name": "vm-yyyy"},
+  --   "trusted_certs": null
+  -- }
+
+
+  -- makeLenses ''PublicKeys
+  -- instance ToJSON PublicKeys where
+  --   toJSON (PublicKeys keys) =
+  --       object ["public-keys" .= fmap createEntry indexedKeys]
+  --       where
+  --         indexedKeys :: [(Int,Text)]
+  --         indexedKeys = zip [0..(length keys)] keys
+  --         createEntry :: (Int, Text) -> Value
+  --         createEntry (index, key) = object [(Text.pack . show) index .= object ["openssh-key" .= key]]
---
+++ test/CPI/Base/AgentConfigSpec.hs
@@ -0,0 +1,126 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module CPI.Base.AgentConfigSpec(spec) where
+
+import           Test.Hspec
+
+import           CPI.Base.AgentConfig
+
+import           Data.ByteString       (ByteString)
+import qualified Data.ByteString       as ByteString
+import qualified Data.ByteString.Char8
+import           Data.ByteString.Lazy  (toStrict)
+import           Data.HashMap.Strict   (HashMap)
+import qualified Data.HashMap.Strict   as HashMap
+
+import           Control.Lens
+import           Data.Aeson
+
+import           Data.Aeson.QQ
+import           Text.RawString.QQ
+
+spec :: Spec
+spec = do
+  describe "parseSettings" $ do
+    it "should parse settings" $ do
+      let input = [r|
+      {
+        "agentId": "agent-id",
+        "blobstore": {},
+        "disks": {
+          "system": "/dev/vda",
+          "ephemeral": "/var/vcap/data",
+          "persistent": {
+            "disk1":"/var/vcap/bosh/persistent/disk1"
+          }
+        },
+        "env": {},
+        "networks": {},
+        "ntp": [],
+        "mbus": "mbus",
+        "vm": {"name": "vm-name"},
+        "trusted_certs": null
+      }
+      |]
+      settings <- parseSettings (Data.ByteString.Char8.pack input)
+      settings `shouldBe` AgentSettings {
+              _agentId = AgentId "agent-id"
+            , _blobstore = Blobstore HashMap.empty
+            , _disks = Just Disks {
+                  _system = "/dev/vda"
+                , _ephemeral = Just "/var/vcap/data"
+                , _persistent = HashMap.singleton "disk1" "/var/vcap/bosh/persistent/disk1"
+              }
+            , _env = HashMap.empty
+            , _networks = HashMap.empty
+            , _ntp = []
+            , _mbus = "mbus"
+            , _vm = Vm {
+                _name = "vm-name"
+              }
+            , _trustedCerts = Nothing
+        }
+    it "parsing and then rendering should yield the original settings" $ do
+      let Success settings = fromJSON [aesonQQ|
+        {
+          "agentId": "agent-xxxxxx",
+          "blobstore": {
+            "provider": "local",
+            "options": {
+              "endpoint": "http://xx.xx.xx.xx:25250",
+              "password": "password",
+              "blobstore_path": "/var/vcap/micro_bosh/data/cache",
+              "user": "agent"
+            }
+          },
+          "disks": {
+            "system": "/dev/xvda",
+            "ephemeral": "/dev/sdb",
+            "persistent": {}
+          },
+          "env": {},
+          "networks": {
+            "default": {
+              "type": "manual",
+              "ip": "10.234.228.158",
+              "netmask": "255.255.255.192",
+              "cloud_properties": {"name": "3112 - preprod - back"},
+              "dns": [
+                "10.234.50.180",
+                "10.234.71.124"
+              ],
+              "gateway": "10.234.228.129",
+              "mac": null
+            }
+          },
+          "ntp": [],
+          "mbus": "nats://nats:nats-password@yy.yy.yyy:4222",
+          "vm": {"name": "vm-yyyy"},
+          "trusted_certs": null
+        }
+        |]
+      let settingsSerialized = toStrict $ encode $ settings
+      settingsDeserialized <- parseSettings settingsSerialized
+      settingsDeserialized `shouldBe` settings
+
+  describe "addPersistentDisk" $ do
+     it "should add a persistent disk" $ do
+       let diskSettings = addPersistentDisk defaultSettings "disk1" "/var/vcap/store"
+       diskSettings ^. disks._Just.persistent.at "disk1"._Just `shouldBe` "/var/vcap/store"
+
+Success defaultSettings = fromJSON [aesonQQ|
+  {
+    "agentId": "agent-xxxxxx",
+    "blobstore": {},
+    "disks": {
+      "system": "/dev/xvda",
+      "ephemeral": "/dev/sdb",
+      "persistent": {}
+    },
+    "env": {},
+    "networks": {},
+    "ntp": [],
+    "mbus": "nats://nats:nats-password@yy.yy.yyy:4222",
+    "vm": {"name": "vm-yyyy"}
+  }
+  |]
